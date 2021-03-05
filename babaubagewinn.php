c766c4b82b25a594249410f31ace50c12c0fb964
mccrossen <marcel.debray@zwickau-crossen.de>
2021-03-05 09:42:06 +0100

    Initiales Hinzufügen der Skripte

---
+++ babaubagewinn.php
@@ -0,0 +1,62 @@
+<?php
+$babauba_los_url = 'https://babauba.de/gewinnlose';
+$lose_file[] = 'losnummern.txt';
+
+### Babauba URL auslesen ANFANG
+$data = file_get_contents($babauba_los_url);
+$dom = new domDocument;
+@$dom->loadHTML($data);
+$dom->preserveWhiteSpace = false;
+$tables = $dom->getElementsByTagName('table');
+$headlines = $dom->getElementsByTagName('h2');
+
+### Babauba URL auslesen ENDE
+
+### Babauba Lose einlesen ANFANG
+$lose = array();
+#zum testen
+#$lose[] = 37296;
+
+foreach($lose_file as $file){
+	$file_handle = fopen($file, 'r');
+
+	while (!feof($file_handle)) {
+		$lose[] = trim(fgets($file_handle));
+	}
+	
+	fclose($file_handle);
+}
+###  Babauba Lose einlesen ENDE
+
+echo "Anzahl der <a href='babaubalose.php'>Lose</a>: ".count($lose)."<br>";
+for($i=0;$i<$tables->length;$i++){
+	$rows = $tables->item($i)->getElementsByTagName('tr');
+	$head = $headlines->item($i);
+	echo utf8_decode($head->nodeValue);
+	$firstrow = true;
+	
+	foreach ($rows as $row) {
+		$cols = $row->getElementsByTagName('td');
+		
+		if($firstrow){
+			$loscol = 0;
+			foreach($cols as $col){
+				if(trim($col->nodeValue) == "Losnummer"){ break;}
+				$loscol++;
+			}
+			$firstrow = false;
+		}
+        #echo $loscol;
+		
+		$wert = trim($cols[$loscol]->nodeValue);
+		#if($i==9) {print_r($cols);}
+        if(in_array($wert,$lose)) {
+			echo " <b>GEWONNEN ".$wert."</b>";
+		} else {
+			echo " ".$wert;
+		}
+		
+}
+echo "<br><!--".$i."-->\r\n"; 
+}
+?> 
---
+++ babaubalose.php
@@ -0,0 +1,31 @@
+<?php
+$lose_file[] = 'losnummern.txt';
+
+echo "<a href='babaubagewinn.php'>Losgewinn&uuml;bersicht</a>";
+
+### Babauba Lose einlesen ANFANG
+$lose = array();
+#zum testen
+#$lose[] = 37296;
+
+foreach($lose_file as $file){
+	$file_handle = fopen($file, 'r');
+
+	echo "Aktuelle Lose aus Datei: ".basename($file)."<br/ >\r\n";
+	$lnbr = 1;
+	$count = 0;
+	while (!feof($file_handle)) {
+	  $line = trim(fgets($file_handle));
+	  $lose[] = $line;
+	  echo $line ." ";
+	  $lnbr++;
+	  $count++;
+	  if($lnbr == 11){
+		$lnbr = 1;
+		echo "<br/ >\r\n";
+	  }
+	}
+	echo "<br/>Anzahl: ".$count."<br/><br/>";
+}
+###  Babauba Lose einlesen ENDE
+?>
